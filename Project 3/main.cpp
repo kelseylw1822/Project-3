1fb17c9e3ff766ce51bffe956714457b47bcbbe5
kelseylw1822 <kelseylw1822@gmail.com>
2025-03-30 17:09:36 -0400

    Initial Commit

---
+++ Project 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project 3
+//
+//  Created by Kelsey Wessel on 3/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
